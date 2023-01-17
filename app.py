abd8b0286a6c31914728678f77290a9b1fdb730a
Ostaptsov Danil <dost4657@gmail.com>
2023-01-17 21:32:34 +0300

    последние изменения

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import gradio as gr
 import requests
 import torch
 from PIL import Image
+from flash.image import ImageClassifier
 
 import lightning as L
 from lightning.app.components.serve import ServeGradio
@@ -11,7 +12,7 @@ from lightning.app.components.serve import ServeGradio
 class LitGradio(ServeGradio):
 
     inputs = gr.inputs.Image(type="pil", source="canvas", label="Draw a glyph")
-    outputs = gr.outputs.Label()
+    outputs = gr.outputs.Label(label="Prediction")
     #demo_img = "https://imageio.forbes.com/specials-images/imageserve/60abf319b47a409ca17f4a3f/Pedestrians-cross-Broadway--in-the-SoHo-neighborhood-in-New-York--United-States--May/960x0.jpg?format=jpg&width=960"
     #img = Image.open(requests.get(demo_img, stream=True).raw)
     #img.save("960x0.jpg")
@@ -27,7 +28,7 @@ class LitGradio(ServeGradio):
         return Image.fromarray(results.ims[0])
     
     def build_model(self):
-        model = torch.hub.load("image_classification_model.pt", "image_classification_model")
+        model = ImageClassifier.load_from_checkpoint("image_classification_model.pt")
         self.ready = True
         return model
 
